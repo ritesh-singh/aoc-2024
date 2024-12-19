126eb013f027039c565cc13058df1adabc6b9b42
Ritesh Kumar Singh <ritesh@Riteshs-MacBook-Pro.local>
2024-12-19 09:46:53 +0530

    Day 17 part 2

--- src/day17/Day17.kt
+++ src/day17/Day17.kt
@@ -1,7 +1,6 @@
 package day17
 
 import Env
-import com.sun.org.apache.xalan.internal.lib.ExsltMath.power
 import println
 import readInput
 import kotlin.math.abs
@@ -62,9 +61,82 @@ private class ChronospatialComputer(env: Env = Env.TEST) {
 
         return result.joinToString(",")
     }
+
+    /**
+     * Combo operand:
+     * 4 -> A
+     * 5 -> B
+     * 6 -> C
+     */
+
+    /**
+     * 0,3, 5,4, 3,0
+     */
+//    private fun reverseEngg(oldA:Int):Pair<Int,Int> {
+//        var a = oldA
+//        // 0,3
+//        a = a shr 3
+//
+//        // 5,4
+//        val out = a and 7
+//
+//        return Pair(a,out)
+//    }
+
+
+    /**
+     * 2,4, 1,7, 7,5, 0,3, 4,0, 1,7, 5,5, 3,0
+     * It has to be long for Part2 :D
+     */
+    private fun reverseEngg(oldA:Long):Pair<Long,Long> {
+        var a = oldA
+
+        // 2 & 4
+        var b = a and 7L
+
+        // 1 & 7
+        b = b xor 7
+
+        // 7 & 5
+        var c = a shr b.toInt()
+
+        // 0,3
+        a = a shr 3
+
+        // 4,0
+        b = b xor c
+
+        // 1,7
+        b = b xor 7
+
+        // 5,5
+        val out = b and 7
+
+        // 3,0
+        return a to out
+    }
+
+    fun part2():Long{
+        val queue  = ArrayDeque<Pair<Int,Long>>()
+        queue.add(program.size to 0L)
+
+        while (queue.isNotEmpty()) {
+            val (cindex, cA) = queue.removeFirst()
+            if (cindex == 0) return cA
+            val instruction = program[cindex - 1].toLong()
+
+            for (i in 0L..7L){
+                val (newA, output) = reverseEngg((cA shl 3) or i)
+                if (output == instruction && newA == cA)
+                    queue.add(cindex - 1 to ((newA shl 3) or i))
+            }
+        }
+        throw IllegalStateException("Should not happen")
+    }
 }
 
 fun main() {
     val env = Env.REAL
     ChronospatialComputer(env = env).programOutput().println()
+    ChronospatialComputer(env = env).part2().println()
 }
